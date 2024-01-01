120df40d21d246d32bb0b53574d192d4b996393c
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-01-01 12:43:56 +0800

    change map location

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
         </div>
         <div className="overflow-hidden rounded-md border border-stone-300 shadow">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d971.3096274556937!2d124.88905742752227!3d-9.111819731911137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2cffbe73c8290341%3A0x3d36f5d737bcb6b0!2sRumah%20Makan%2099%20Kyu%20Kyu!5e0!3m2!1sen!2sid!4v1704083274621!5m2!1sen!2sid"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d984.8682389143966!2d124.88798676957896!3d-9.111681975252184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2cffbf2530db5a03%3A0xde8a4862220db849!2sRumah%20Makan%20Am%20Pm!5e0!3m2!1sen!2sid!4v1704084193224!5m2!1sen!2sid"
             width="400"
             height="250"
             allowFullScreen={true}
