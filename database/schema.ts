1c80bd5fbba28a52a06d2bd4d60aacca8dbc9d07
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-01-31 00:27:28 +0700

    update schema

--- database/schema.ts
+++ database/schema.ts
@@ -17,8 +17,30 @@ export const orderLines = sqliteTable("order_lines", {
   orderId: integer("order_id").references(() => orders.id),
 });
 
+export const categories = sqliteTable("categories", {
+  id: integer("id").primaryKey(),
+  name: text("name"),
+  order: integer("order"),
+});
+
+export const menu = sqliteTable("menu", {
+  id: integer("id").primaryKey(),
+  name: text("name"),
+  description: text("description"),
+  price: integer("price"),
+  image: text("image"),
+  order: integer("order"),
+  categoryId: integer("category_id").references(() => categories.id),
+});
+
 export type Order = InferSelectModel<typeof orders>;
 export type NewOrder = InferInsertModel<typeof orders>;
 
 export type OrderLine = InferSelectModel<typeof orderLines>;
 export type NewOrderLine = InferSelectModel<typeof orderLines>;
+
+export type MenuLine = InferSelectModel<typeof menu>;
+export type NewMenuLine = InferSelectModel<typeof menu>;
+
+export type CategoryLine = InferSelectModel<typeof categories>;
+export type NewCategoryLine = InferSelectModel<typeof categories>;
