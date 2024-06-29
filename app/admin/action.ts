a92528e0cbe3e0756e2a7548b729000ad9da9621
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-06-30 01:17:56 +0700

    fix directive lol

--- app/admin/action.ts
+++ app/admin/action.ts
@@ -3,7 +3,6 @@
 import { login } from "./auth";
 
 export async function onLogin(_prevState: any, formData: FormData) {
-  "use server";
   const passcode = formData.get("passcode");
   const error = login(passcode?.toString());
   return { error };
